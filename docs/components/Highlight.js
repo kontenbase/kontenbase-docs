7de93f1b795fc4caca0d1ddcc6d127e2ecf75734
M Haidar Hanif <git@mhaidarhanif.com>
2022-01-27 04:32:31 +0700

    chore: format js

--- docs/components/Highlight.js
+++ docs/components/Highlight.js
@@ -1,13 +1,14 @@
-const Highlight = ({children, color}) => (
-    <span
-      style={{
-        backgroundColor: color,
-        borderRadius: '2px',
-        color: '#fff',
-        padding: '0.2rem',
-      }}>
-      {children}
-    </span>
-);
+const Highlight = ({ children, color }) => (
+  <span
+    style={{
+      backgroundColor: color,
+      borderRadius: '2px',
+      color: '#fff',
+      padding: '0.2rem',
+    }}
+  >
+    {children}
+  </span>
+)
 
-export const PostMethod = ()=> (<Highlight color="#25c2a0">POST</Highlight>);
+export const PostMethod = () => <Highlight color="#25c2a0">POST</Highlight>
